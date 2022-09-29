df79a468e382ab5295c5f4482ed15db01c6624d0
Jared Polonitza <jpolonitza@primetrust.com>
2022-09-28 21:49:07 -0700

    Add key for telescope

--- config/nvim/lua/keys.lua
+++ config/nvim/lua/keys.lua
@@ -2,4 +2,5 @@
 local map = vim.api.nvim_set_keymap
 
 -- Toggle nvim-tree
-map('n', 'n', [[:NvimTreeToggle]], {})
+map('n', 'tr', [[:NvimTreeToggle]], {})
+map('n', 'tel', [[:Telescope find_files]], {})
