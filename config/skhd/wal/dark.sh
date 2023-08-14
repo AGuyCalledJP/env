de7e21677a4e4e14ca9f5e382aa245df7647764f
Jared Polonitza <jaredp1234@gmail.com>
2023-08-14 15:47:30 -0700

    Add python script to bridge cross display functionality

--- config/skhd/wal/dark.sh
+++ config/skhd/wal/dark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wal --backend haishoku -i ${HOME}/.env/config/backgrounds/forest.jpg -n -q 
+wal --backend haishoku -i ${HOME}/.env/config/backgrounds/$(cat ${HOME}/.env/config/bg-image) -n -q 
 echo 0 > ${HOME}/.env/config/lightordark
--- config/skhd/wal/light.sh
+++ config/skhd/wal/light.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wal --backend haishoku -i ${HOME}/.env/config/backgrounds/forest.jpg -n -q -l
+wal --backend haishoku -i ${HOME}/.env/config/backgrounds/$(cat ${HOME}/.env/config/bg-image) -n -q -l
 echo 1 > ${HOME}/.env/config/lightordark
