d743fc6739a4f9a0c74fa3b28e1f2d2fd3f524ad
Jared Polonitza <jaredp1234@gmail.com>
2023-08-21 09:36:31 -0700

    ps-code v1.0.1

--- config/scripts/polo-code-create.sh
+++ config/scripts/polo-code-create.sh
@@ -1,6 +1,17 @@
+#!/usr/bin/env bash
+
+yabai -m window --focus west
+yabai -m window --stack next
 selected_name=$(cat ~/.env/lastusedmux)
+
 session_count=$(tmux ls | grep $selected_name | wc -l | xargs)
-new_session_name=$(echo $selected_name)_$(echo $session_count)
+attached_session_count=$(tmux ls | grep $selected_name | grep attached | wc -l | xargs)
+
+min_count=$(( session_count < attached_session_count ? session_count : attached_session_count ))
+
+new_session_name=$(echo $selected_name)_$(echo $min_count)
 tmux new-session -A -d  -s $new_session_name -t $selected_name >> /dev/null
-tmux new-window -a -t $selected_name -c $(cat ~/.env/lastdir)
-tmux attach -t $new_session_name:$session_count 
+if [[ $min_count -ge $attached_session_count ]]; then
+  tmux new-window -a -t $selected_name -c $(cat ~/.env/lastdir)
+fi 
+tmux attach -t $new_session_name:$min_count
--- config/scripts/polo-code.sh
+++ config/scripts/polo-code.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 ~/.env/config/scripts/launch-terminal.py
-~/.env/config/scripts/launch-terminal.py
-~/.env/config/scripts/launch-terminal.py
-
-yabai -m window --focus north 
-yabai -m window --stack next
-yabai -m window --stack next
 
 yabai -m space --rotate 270
 
