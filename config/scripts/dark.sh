516440578b71ee97e3843c0edcf9c7bd3faf85c4
Jared Polonitza <jaredp1234@gmail.com>
2023-09-06 10:33:57 -0700

    Fix bug for color scheme, remove haikushou, add new backgrounds

--- config/scripts/dark.sh
+++ config/scripts/dark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wal --backend haishoku -i ${HOME}/.env/config/backgrounds/$(cat ${HOME}/.env/config/bg-image) -n -q 
-echo 0 > ${HOME}/.env/config/lightordark
+wal -i ~/.env/config/backgrounds/$(cat ~/.env/background) -n -q 
+echo 0 > ${HOME}/.env/lightordark
--- config/scripts/light.sh
+++ config/scripts/light.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wal --backend haishoku -i ${HOME}/.env/config/backgrounds/$(cat ${HOME}/.env/config/bg-image) -n -q  -l
-echo 0 > ${HOME}/.env/config/lightordark
+wal -i ~/.env/config/backgrounds/$(cat ~/.env/background) -n -q -l
+echo 1 > ${HOME}/.env/lightordark
